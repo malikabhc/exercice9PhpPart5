a6840c240acab767c13f31a1b12fb9305d1361d5
Malikabhc <malika.bouhcine@gmail.com>
2018-07-31 09:30:31 +0200

    exercice9-5

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 9/5</title>
+  </head>
+  <body>
+    <p>
+      <?php
+// Création du tableau $region
+        $region = array (
+          '02' => 'Aisne',
+          // Se lit : Index 02 associé à Aisne.
+          '59' => 'Nord',
+          '60' => 'Oise',
+          '62' => 'Pas-de-Calais',
+          '80' => 'Somme');
+        // Afficher le tableau à l'aide d'une boucle foreach (plus adaptée aux tableaux associatifs)
+        foreach ($region as $key => $element) {
+          echo $key . ' ' . $element . ' ';
+        }
+      ?>
+    </p>
+  </body>
+</html>
